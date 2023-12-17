1fe0d90c9630ad4ef7f3c94b4623d405bdc4c85a
Abdiqani <abdiqaniadan254@gmail.com>
2023-12-18 00:12:02 +0300

    Resturant class added

---
+++ lib/resturant.py
@@ -0,0 +1,11 @@
+class Resturant:
+    def __init__(self, name="Kibandaski"):
+        self.name =name
+        
+
+    def name(self):
+        return self.name
+    
+
+reaturant=Resturant("Kibandaski")
+print (reaturant.name)
