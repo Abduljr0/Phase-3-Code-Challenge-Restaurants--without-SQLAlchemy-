e056e139372c63aebfb35caccddffc1bb6876b69
Abdiqani <abdiqaniadan254@gmail.com>
2023-12-18 12:53:02 +0300

    reviews added

--- lib/customer.py
+++ lib/customer.py
@@ -82,6 +82,30 @@ class Restaurant:
     @classmethod
     def all(cls):
         return cls.restaurants
+    
+class Review:
+    all_reviews = []
+
+    def __init__(self, customer, restaurant, rating):
+        self._customer = customer
+        self._restaurant = restaurant
+        self._rating = rating
+
+        Review.all_reviews.append(self)
+
+    def customer(self):
+        return self._customer
+
+    def restaurant(self):
+        return self._restaurant
+
+    def rating(self):
+        return self._rating
+
+    @classmethod
+    def all(cls):
+        return cls.all_reviews
+    
 
 
 
