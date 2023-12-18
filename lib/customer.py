e6702a90f5cce327a7b28019b5558ec7d78c4899
Abdiqani <abdiqaniadan254@gmail.com>
2023-12-18 12:49:33 +0300

    resturant added

--- lib/customer.py
+++ lib/customer.py
@@ -55,6 +55,35 @@ customer1._first_name = "Khalid"
 #customers= Customer.all()
 print(customer3.first_name())
 
+class Restaurant:
+    restaurants = []
+
+    def __init__(self, name):
+        self._name = name
+        self._reviews = []
+        Restaurant.restaurants.append(self)
+
+    def name(self):
+        return self._name
+    
+    def reviews(self):
+        return self._reviews
+
+    def customers(self):
+        return list(set(review.customer() for review in self._reviews))
+    
+    def average_star_rating(self):
+        if not self._reviews:
+            return 0
+
+        total_rating = sum(review.rating() for review in self._reviews)
+        return total_rating / len(self._reviews)
+
+    @classmethod
+    def all(cls):
+        return cls.restaurants
+
+
 
 
 
